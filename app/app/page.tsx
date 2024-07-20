3307470835fe6b7ad063d91e5562f5ce6d6b1ffa
Rohith Singh <singhmalik583@gmail.com>
2024-07-20 10:08:05 +0530

    auth wrap

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -1,7 +1,21 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useCurrentUser } from "@/hooks/use-current-user";
+import { signOut } from "next-auth/react";
+
 const MainPage = () => {
+
+    const handleSignOut = () => {
+        signOut();
+    }
+
+    const user = useCurrentUser();
+
     return ( 
         <div>
-            MAin page
+            <h1>Welcome {user?.email}</h1>
+            <Button onClick={handleSignOut}>Sign out</Button>
         </div>
      );
 }
